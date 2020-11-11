642d55eb1bd65ced01d5d0f7160ebce55b2bbbcc
Roman Vanesyan <roman@refactr.it>
2020-11-11 15:57:58 +0500

    override help message

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -8,6 +8,20 @@ import { BASE_PATH } from '../constants';
 
 declare const __VERSION__: string;
 
+const helpMessage = `
+Usage: refactrctl [options] [command]
+
+Options:
+  -V, --version           output the version number
+  --access-token <token>  API access token, can also be specified via REFACTR_AUTH_TOKEN environment variable
+  --api-url <url>         API endpoint (default: "${BASE_PATH}")
+  -h, --help              display help for command
+
+Commands:
+  job [options]           job actions
+  project                 project actions
+`;
+
 const createProgram = () => {
     const program = new Command();
 
@@ -16,10 +30,12 @@ const createProgram = () => {
         .name('refactrctl')
         .option(
             '--access-token <token>',
-            'API access token',
+            'API access token, can also be specified via REFACTR_AUTH_TOKEN environment variable',
             process.env.REFACTR_AUTH_TOKEN
         )
         .option('--api-url <url>', 'API endpoint', BASE_PATH);
+    program.helpInformation = (): string => helpMessage;
+    program.addHelpCommand(false);
 
     // @ts-ignore
     applyCommands(program, [job, project]);
