0d572846afb4d7389611221a0c868ca340b78f8c
refactr_anthony <anthony@refactr.it>
2020-11-12 13:54:22 -0800

    prevent CLI from printing env var option defaults

--- src/cmd/project.ts
+++ src/cmd/project.ts
@@ -4,16 +4,19 @@ import { list } from '../actions/project';
 const applyCommand = (program: Command) => {
     const command = new Command('project');
 
-    command.description('project actions');
+    command.description('Commands to view and modify projects.');
 
     command
         .command('list')
         .option('--organization <id>', 'Filter by organization')
-        .description('list available projects')
+        .description('List projects. If --organization is provided, the list is filtered by the organization ID.')
         .action((cmd: Command) => {
             const organization = cmd.organization;
             const parent = cmd.parent;
-            const accessToken = parent.parent.accessToken;
+            const accessToken = parent.parent.accessToken ?? process.env.REFACTR_AUTH_TOKEN;
+            if (!accessToken) {
+                throw new Error('Auth token is required.');
+            };
             const basePath = parent.parent.apiUrl;
 
             return list(accessToken, basePath, {
