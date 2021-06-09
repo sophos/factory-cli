8dabc4f6b9e380e2a264ef37d991c13b0a88a938
Roman Vanesyan <roman@refactr.it>
2021-06-09 23:59:19 +0500

    feat: add support for limit, offset and sort parameters.

--- src/cmd/list/pipelines.ts
+++ src/cmd/list/pipelines.ts
@@ -1,25 +1,17 @@
+import { SharedArguments } from './shared-arguments';
 import fields from '../../fields';
 import { createCommandResult, handler } from '../handler';
 
-type Arguments = {
+type Arguments = SharedArguments & {
   projectId: string;
   sort?: 'modified_asc' | 'modified_desc' | 'name_asc' | 'name_desc';
-  limit?: number;
-  offset?: number;
-  search?: string;
 };
 
 export default handler(
-  async (apiClient, { projectId, sort, limit, offset, search }: Arguments) => {
+  async (apiClient, { projectId, sort, limit, offset }: Arguments) => {
     const api = apiClient.pipelines;
 
-    const { data } = await api.listPipelines(
-      projectId,
-      sort,
-      limit,
-      offset,
-      search
-    );
+    const { data } = await api.listPipelines(projectId, sort, limit, offset);
     const list = data?.pipelines ?? [];
 
     return createCommandResult('view', list, fields.pipeline);
--- src/cmd/list/projects.ts
+++ src/cmd/list/projects.ts
@@ -1,19 +1,30 @@
 import isNil from 'lodash/isNil';
+
 import fields from '../../fields';
-
 import { createCommandResult, handler } from '../handler';
+import { SharedArguments } from './shared-arguments';
 
-type Arguments = {
+type Arguments = SharedArguments & {
   organizationId?: string;
+  sort?: 'modified_asc' | 'modified_desc' | 'name_asc' | 'name_desc';
 };
 
 export default handler(
-  async (apiClient, { organizationId }: Arguments = {}) => {
+  async (
+    apiClient,
+    { organizationId, offset, limit, sort }: Arguments = {}
+  ) => {
     const api = apiClient.projects;
 
     const { data } = !isNil(organizationId)
-      ? await api.listOrganizationProjects(organizationId)
-      : await api.listProjects();
+      ? await api.listOrganizationProjects(
+          organizationId,
+          sort,
+          void 0,
+          offset,
+          limit
+        )
+      : await api.listProjects(sort, void 0, limit, offset);
     const list = data?.projects ?? [];
 
     return createCommandResult('view', list, fields.project);
