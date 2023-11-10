4a2e38c8d018c0f34a9b922d52ea0046479865a2
Rick Culpepper <rick.culpepper@sophos.com>
2023-11-10 13:53:00 -0600

    Update Known IDs to use 'Factory CLI local testing' project

--- tests/helpers/knownIds.ts
+++ tests/helpers/knownIds.ts
@@ -1,11 +1,11 @@
 const knownIds = {
   organization: '5f9f2d08e2f1bb2436e6f5b0', // "Refactr"
-  project: '5fa4dba223b4e0703b95c465', // "Full Kitchen Sink"
-  job: '632a8b28630394e2de3b7bd3', // "Manually run the Scheduled KS pipeline"
-  pipeline: '5fb86500bd71fd7b2b2e9cd0', // "S1 - All"
-  runner: '6259e10b3e6c5ef6ea798f67', // "Kitchen Sink Sync Runner"
-  credential: 't3-fake-bearer-token', // "t3-fake-bearer-token"
-  pipelineRevision: '63f68ee728be4a69cfec5b47' // "Auto-created from pipeline sync script"
+  project: '6477b15f5e69034001017c0c', // "Factory CLI local testing"
+  job: '64791928a7008c51fd4c7980', // "How Do We Know It Works?"
+  pipeline: '6479166fa7008c51fd4c7747', // "The Answer is Always Testing"
+  runner: '64791a7da7008c51fd4c79f5', // "Factory CLI Test Runner"
+  credential: 'secret-test-cred', // "Super Secret Test Credential"
+  pipelineRevision: '6479166fa7008c51fd4c7749' // "Initial Revision"
 };
 
 export default knownIds;
